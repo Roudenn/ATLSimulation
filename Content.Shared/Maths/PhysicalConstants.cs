8ab07364f32277cdb812321f7db40acb49c77a62
Roudenn <romabond091@gmail.com>
2026-01-14 21:46:31 +0300

    Port Temperature Containers

---
+++ Content.Shared/Maths/PhysicalConstants.cs
@@ -0,0 +1,10 @@
+namespace Content.Shared.Maths;
+
+/// <summary>
+///     Contains physical constants used in calculations.
+/// </summary>
+public static class PhysicalConstants
+{
+    public const float ZERO_CELCIUS = 273.15f;
+    public const float ROOM_TEMPERATURE = ZERO_CELCIUS + 20f;
+}
