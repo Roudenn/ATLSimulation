b272ebc25b124921b59231a3e48a7cb295f0fb91
Roudenn <romabond091@gmail.com>
2026-02-14 11:56:28 +0300

    Delete observers after player is detached

---
+++ Content.Server/Player/ObserverSystem.cs
@@ -0,0 +1,19 @@
+﻿using Content.Shared.Player;
+using Robust.Shared.Player;
+
+namespace Content.Server.Player;
+
+public sealed class ObserverSystem : EntitySystem
+{
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<ObserverComponent, PlayerDetachedEvent>(OnDetached);
+    }
+
+    private void OnDetached(Entity<ObserverComponent> ent, ref PlayerDetachedEvent args)
+    {
+        QueueDel(ent.Owner);
+    }
+}
---
+++ Content.Shared/Player/ObserverComponent.cs
@@ -0,0 +1,9 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared.Player;
+
+/// <summary>
+/// Marks this entity as an observer, so it will delete after a player was detached from it.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class ObserverComponent : Component;
