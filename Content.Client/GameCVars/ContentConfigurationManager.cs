c88285b14c84f35c2cd63d5d8bd75a2ab810af92
Roudenn <romabond091@gmail.com>
2026-04-15 19:07:42 +0300

    Update ContentConfigurationManager.cs

--- Content.Client/GameCVars/ContentConfigurationManager.cs
+++ Content.Client/GameCVars/ContentConfigurationManager.cs
@@ -1,4 +1,5 @@
-﻿using Robust.Shared.Configuration;
+﻿using System.Globalization;
+using Robust.Shared.Configuration;
 using Robust.Shared.Console;
 
 namespace Content.Client.GameCVars;
@@ -7,6 +8,8 @@ public sealed class ContentConfigurationManager
 {
     [Dependency] private readonly IConsoleHost _conHost = default!;
 
+    private ISawmill _sawmill = Logger.GetSawmill("content_config");
+
     public void SetCVar(string name, object value)
     {
         _conHost.ExecuteCommand($"sudo cvar {name} {value}");
@@ -14,6 +17,17 @@ public sealed class ContentConfigurationManager
 
     public void SetCVar<T>(CVarDef<T> def, T value) where T : notnull
     {
-        _conHost.ExecuteCommand($"sudo cvar {def.Name} {value.ToString()}");
+        _sawmill.Info($"Default: {value.ToString()}");
+        if (value is IFormattable format)
+        {
+            var culture = format.ToString(null, CultureInfo.InvariantCulture);
+            _sawmill.Info($"With Culture: {culture}");
+            _conHost.ExecuteCommand($"sudo cvar {def.Name} {culture}");
+        }
+        else
+        {
+            _conHost.ExecuteCommand($"sudo cvar {def.Name} {value}");
+            _sawmill.Info($"Fallback: {value.ToString()}");
+        }
     }
 }
